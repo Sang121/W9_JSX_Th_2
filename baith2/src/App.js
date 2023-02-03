c1304b93ee681c3231398f8298e83c315a3cb1bd
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-03 21:11:58 +0700

    Bai thuc hanh 2

---
+++ baith2/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './header'
+import Footer from './footer'
+
+function App() {
+  return (
+   <div>
+    <Header/>
+    <div>đây là body</div>
+    <Footer/> 
+    </div>
+   
+  )
+}
+
+export default App;
